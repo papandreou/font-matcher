4e3f6fa5fac80d1cee310e4bd30c4e8702f8d6e2
Andreas Lind <andreaslindpetersen@gmail.com>
2019-03-18 23:11:27 +0100

    Move findDistinctTraceGroupSets to its own file, clean up

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const simulatedAnnealing = require('./lib/simulatedAnnealing');
 const getWordPositions = require('./lib/getWordPositions');
 const findClosestWebSafeFont = require('./lib/findClosestWebSafeFont');
 const shorthandify = require('./lib/shorthandify');
+const findDistinctTraceGroupSets = require('./lib/findDistinctTraceGroupSets');
 const fontFamilyParser = require('font-family-papandreou');
 const { pickone, integer } = require('chance-generators');
-const SuperCollider = require('./lib/SuperCollider');
 
 const fontRelatedProps = [
   'font-family',
@@ -303,57 +303,12 @@ async function optimize(page, traceGroups) {
       );
     }
 
-    const superCollider = new SuperCollider(traceGroups);
+    const distinctTraceGroupSets = await findDistinctTraceGroupSets(
+      traceGroups,
+      page
+    );
 
-    // Find out which trace groups affect the positioning of others
-    for (const traceGroup of traceGroups) {
-      const fontSize = `${4 * parseInt(traceGroup.computedStyle.fontSize)}px`;
-      const lineHeight = `${4 *
-        parseInt(traceGroup.computedStyle.lineHeight)}px`;
-
-      const oldStyles = await page.evaluate(
-        (fontSize, lineHeight, ...elements) => {
-          const oldStyles = [];
-          for (const element of elements) {
-            oldStyles.push([element.style.fontSize, element.style.lineHeight]);
-            element.style.fontSize = fontSize;
-            element.style.lineHeight = lineHeight;
-          }
-          return oldStyles;
-        },
-        fontSize,
-        lineHeight,
-        ...traceGroup.elementHandles
-      );
-
-      for (const otherTraceGroup of traceGroups) {
-        if (!superCollider.haveCollision(traceGroup, otherTraceGroup)) {
-          const wordPositionsNow = await Promise.all(
-            otherTraceGroup.elementHandles.map(elementHandle =>
-              getWordPositions(page, elementHandle)
-            )
-          );
-
-          if (
-            !_.isEqual(otherTraceGroup.referenceWordPositions, wordPositionsNow)
-          ) {
-            superCollider.registerCollision(traceGroup, otherTraceGroup);
-          }
-        }
-      }
-      await page.evaluate(
-        (oldStyles, ...elements) => {
-          for (const [i, element] of elements.entries()) {
-            element.style.fontSize = oldStyles[i][0];
-            element.style.lineHeight = oldStyles[i][1];
-          }
-        },
-        oldStyles,
-        ...traceGroup.elementHandles
-      );
-    }
-
-    for (const distinctTraceGroupSet of superCollider) {
+    for (const distinctTraceGroupSet of distinctTraceGroupSets) {
       await optimize(page, [...distinctTraceGroupSet]);
     }
   } finally {
